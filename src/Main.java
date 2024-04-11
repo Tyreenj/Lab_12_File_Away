6a13fdbffc226bb60cadedb5c1513a3f45156a0b
Noah Tyree <noahjtyree@gmail.com>
2024-04-11 12:27:33 -0400

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,79 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int lineCounter = 0;
+        JFileChooser chooser = new JFileChooser();
+        Scanner inFile;
+        String line;
+        int character = 0;
+        int words = 0;
+        String fileName;
+        Path target = new File(System.getProperty("user.dir")).toPath();
+        target = target.resolve("src");
+        // set the chooser to the project src directory
+        chooser.setCurrentDirectory(target.toFile());
+
+        try  // Code that might trigger the exception goes here
+        {
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                target = chooser.getSelectedFile().toPath();  // this is a File object not a String filename
+
+                inFile = new Scanner(target);
+
+                while(inFile.hasNextLine())
+                {
+                    lineCounter++;
+                    line = inFile.nextLine();
+                    System.out.println(line);
+                    character = character + line.length();
+
+                    words = words + line.split("\\s").length;
+                }
+
+                System.out.println();
+
+                try {
+                    fileName = target.toFile().getName();
+                    System.out.println("The name of the file is" +fileName);
+                }
+                catch (Exception e)
+                {
+                    System.err.println(e.getMessage());
+                }
+
+
+                inFile.close();
+
+                System.out.println("There are " +lineCounter +" lines in this file.");
+                System.out.println("There are " +words +" words in this file.");
+                System.out.println("There are " +character +" characters in this file.");
+
+            }
+            else   // User did not pick a file, closed the chooser
+            {
+                System.out.println("Sorry, you must select a file! Termininating!");
+                System.exit(0);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File Not Found Error");
+            e.printStackTrace();
+        }
+        catch (IOException e) // code to handle this exception
+        {
+            System.out.println("IOException Error");
+            e.printStackTrace();
         }
     }
 }
